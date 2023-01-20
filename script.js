dc50b29e96313addd4e88388f34fb9ce89fb6331
Nicolas Moraes <62454946+moraes7@users.noreply.github.com>
2023-01-20 10:17:31 -0300

    aula 4

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 const form = document.querySelector('#form-habits');
 const nlwSetup = new NLWSetup(form);
+const button = document.querySelector('header button');
 
-const data = {
-    run: ['01-01', '01-03', '01-05', '01-07'],
-    gym: ['01-02', '01-04', '01-06'],
-    water: ['01-01', '01-02', '01-03', '01-04', '01-05', '01-06', '01-07'],
-    food: ['01-01', '01-02', '01-03', '01-04', '01-05', '01-06', '01-07'],
-    sleep: ['01-01', '01-02', '01-03', '01-04', '01-05', '01-06', '01-07'],
-    study: ['01-02', '01-03', '01-04', '01-05', '01-06']
+button.addEventListener('click', add);
+form.addEventListener('change', save);
+
+function add() {
+    const today = new Date().toLocaleDateString('pt-br').slice(0, -5);
+    const dayExists = nlwSetup.dayExists(today);
+
+    if(dayExists) {
+        alert('Esse dia já existe! ❌');
+        return
+    }
+    
+    alert('Adicionado com sucesso ✅');
+    nlwSetup.addDay(today);
 }
 
+function save() {
+    localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data));
+}
+
+
+const data = JSON.parse(localStorage.getItem('NLWSetup@habits')) || {};
 nlwSetup.setData(data);
 nlwSetup.load();
